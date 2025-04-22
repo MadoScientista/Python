40c3032fb144173b9e1e9dfc600fa071280c6c2a
MadoScientista <samuel.cortes.n@gmail.com>
2025-04-22 09:12:28 -0400

    Actualización comentarios

--- Ejercicios_Basicos/promedio_3_notas.py
+++ Ejercicios_Basicos/promedio_3_notas.py
@@ -2,19 +2,26 @@
 # Calcula promedio
 # Informa si aprobó
 
+
+# Bienvenida
 print("-"*40)
 print("Bienvenido a la calculadora de promedios")
 print("-"*40)
 
+# Solicita datos
+# input recibe String y float() convierte a decimal (float)
 num1 = float(input("Ingresa la primera nota: "))
 num2 = float(input("Ingresa la segunda nota: "))
 num3 = float(input("Ingresa la tercera nota: "))
 
+# Calcula el promedio
 avg = (num1+num2+num3)/3
 
+# Informa al usuario el promedio
 print("-"*30)
 print(f"El promedio entre {num1}, {num2}, {num3} es: {avg}")
 
+# Evalúa si aprobó e informa el resultado
 if avg >= 4.0:
     print("El/La estudiante ha aprobado")
 else:
