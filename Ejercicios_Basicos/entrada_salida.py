8c195b0afdfb4bc39277d4daf3ecb46837c65654
MadoScientista <samuel.cortes.n@gmail.com>
2025-04-22 09:15:42 -0400

    Actualización comentarios

--- Ejercicios_Basicos/entrada_salida.py
+++ Ejercicios_Basicos/entrada_salida.py
@@ -1,6 +1,6 @@
-# Solicita datos al usuario
+# Solicita nombre al usuario
 # Imprime datos por pantalla
 
-nombre = input("Ingresa tu nombre: ")
+nombre = input("Ingresa tu nombre: ") # Input recibe String ingresado
 
 print("hola " + nombre)
