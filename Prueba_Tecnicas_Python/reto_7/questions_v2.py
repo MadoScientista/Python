58cb3db6605898719f0fb48f88fece5bc11b2299
MadoScientista <samuel.cortes.n@gmail.com>
2025-04-25 01:26:11 -0400

    Lista de preguntas

--- Prueba_Tecnicas_Python/reto_7/questions_v2.py
+++ Prueba_Tecnicas_Python/reto_7/questions_v2.py
@@ -1,41 +1,32 @@
 questions = [
-    (""" ¿Qué cualidad valoras más en ti mismo?
-        
-        a) Valentía y osadía
-        b) Ambición y astucia
-        c) Lealtad y trabajo duro
-        d) Inteligencia y curiosidad
-         """),
-
-    (""" ¿Qué tipo de desafío te atrae más?
-
-        a) Enfrentar un peligro por una buena causa
-        b) Superar a mis rivales para alcanzar mis metas
-        c) Colaborar con otros para lograr un objetivo común
-        d) Resolver un enigma complejo o aprender algo nuevo
-          """),
-
-    ("""¿Cómo reaccionarías ante una injusticia?
-
-        a) Me levantaría para defender a los oprimidos, sin importar el riesgo.
-        b) Buscaría la manera más estratégica para sacar provecho de la situación.
-        c) Trabajaría diligentemente para restaurar la armonía y el equilibrio.
-        d) Investigaría a fondo la situación para entenderla completamente antes de actuar.
-          """),
-
-    ("""¿Qué entorno de aprendizaje prefieres?
-
-        a) Uno donde pueda demostrar mi coraje y liderazgo.
-        b) Uno donde pueda poner a prueba mi ingenio y estrategia.
-        c) Uno donde haya un fuerte sentido de comunidad y apoyo mutuo.
-        d) Uno que estimule mi mente con ideas y debates interesantes.
-          """),
-
-
-    ("""¿Qué tipo de criatura mágica te inspira más respeto?
-
-        a) Un grifo, por su nobleza y valentía.
-        b) Una serpiente, por su astucia y poder.
-        c) Un tejón, por su lealtad y dedicación.
-        d) Un águila, por su visión y sabiduría.
-          """)]
+    ("¿Qué cualidad valoras más en ti mismo?", [
+        ("Valentía y osadía", "Gryffindor"),
+        ("Ambición y astucia", "Slytherin"),
+        ("Lealtad y trabajo duro", "Hufflepuff"),
+        ("Inteligencia y curiosidad", "Ravenclaw")
+    ]),
+    ("¿Qué tipo de desafío te atrae más?", [
+        ("Enfrentar un peligro por una buena causa", "Gryffindor"),
+        ("Superar a mis rivales para alcanzar mis metas", "Slytherin"),
+        ("Colaborar con otros para lograr un objetivo común", "Hufflepuff"),
+        ("Resolver un enigma complejo o aprender algo nuevo", "Ravenclaw")
+    ]),
+    ("¿Cómo reaccionarías ante una injusticia?", [
+        ("Me levantaría para defender a los oprimidos, sin importar el riesgo", "Gryffindor"),
+        ("Buscaría la manera más estratégica para sacar provecho de la situación", "Slytherin"),
+        ("Trabajaría diligentemente para restaurar la armonía y el equilibrio", "Hufflepuff"),
+        ("Investigaría a fondo la situación antes de actuar", "Ravenclaw")
+    ]),
+    ("¿Qué entorno de aprendizaje prefieres?", [
+        ("Uno donde pueda demostrar mi coraje y liderazgo", "Gryffindor"),
+        ("Uno donde pueda poner a prueba mi ingenio y estrategia", "Slytherin"),
+        ("Uno donde haya un fuerte sentido de comunidad y apoyo mutuo", "Hufflepuff"),
+        ("Uno que estimule mi mente con ideas y debates interesantes", "Ravenclaw")
+    ]),
+    ("¿Qué criatura mágica te inspira más respeto?", [
+        ("Un grifo, por su nobleza y valentía", "Gryffindor"),
+        ("Una serpiente, por su astucia y poder", "Slytherin"),
+        ("Un tejón, por su lealtad y dedicación", "Hufflepuff"),
+        ("Un águila, por su visión y sabiduría", "Ravenclaw")
+    ])
+]
