5c2c83ad37460ffb9f021b2b25201b1f3662837c
MadoScientista <samuel.cortes.n@gmail.com>
2025-05-20 10:30:57 -0400

    20-05-2025

--- Ejercicios_Basicos/24_pasar_ramo.py
+++ Ejercicios_Basicos/24_pasar_ramo.py
@@ -8,17 +8,40 @@
 # ingrese la nota final y la suma de las décimas cumuladas
 # Muestre si aprueba o reprueba
 
+from random import randint
 
-while n_perros < 1:
+# Inicializa variables necesarias
+n_rojos = 0
+asistencia = 0
+nota = 0
+
+# Pregunta la cantidad de rojos en el curso
+while n_rojos < 1:
     try:
-        n_perros = int(input("Ingrese la cantidad de perros: "))
-        
-        if n_perros < 0:
+        n_rojos = int(input("Ingrese la cantidad de rojos: "))
+
+        if n_rojos < 0:
             print("-"*40)
             print("Ingrese solo números enteros positivos")
             print("-"*40)
-            n_perros = 0
+            n_rojos = 0
         
     except ValueError as error:
         # print(error)
         print("Ingrese solo números enteros positivos")
+
+
+
+for i in range(n_rojos):
+    nota = randint(10, 39)/10
+    asistencia = randint(1, 4)
+
+    print(f"Estudiante n°{i+1} obtuvo un {nota}")
+
+    if asistencia == 4:
+        nota += 1.2
+
+    if nota >= 4:
+        print(f"asistió a {asistencia} talleres, ha aprobado con un {nota}")
+    else:
+        print(f"asistió a {asistencia} talleres, ha reprobado con un {nota}")
